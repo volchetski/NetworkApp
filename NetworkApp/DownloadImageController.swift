fa3e4f66916721cb3289263b884017ca5d611e5c
volchetski <zhenyahd2010@gmail.com>
2022-12-26 15:51:01 +0300

    Added Controller for to show image

--- NetworkApp/DownloadImageController.swift
+++ NetworkApp/DownloadImageController.swift
@@ -8,22 +8,48 @@
 import UIKit
 
 class DownloadImageController: UIViewController {
+    
+    // MARK: - Properties
+    let urlString = "https://applelives.com/wp-content/uploads/2016/03/iPhone-SE-11.jpeg"
+    
+    public lazy var imageView: UIImageView = {
+        let image = UIImageView()
+        image.contentMode = .scaleAspectFit
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        
+        addSubviews()
+        addConstraints()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Methods
+    private func addSubviews() {
+        view.addSubview(imageView)
     }
-    */
-
+    
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            imageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            imageView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+    
+    public func getImage() {
+        guard let url = URL(string: urlString) else { return }
+        let session = URLSession.shared
+        session.dataTask(with: url) { data, response, error in
+            if let data = data, let image = UIImage(data: data) {
+                DispatchQueue.main.async {
+                    self.imageView.image = image
+                }
+            }
+        }.resume()
+    }
 }
